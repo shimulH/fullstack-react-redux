57e84f5604b11ba73b48bd8d605b057b4f7d69fd
shimulH <theshimulh@gmail.com>
2019-07-02 09:59:44 +0600

    Auth Flow change5

--- config/dev.js
+++ config/dev.js
@@ -1,9 +1,11 @@
 module.exports = {
   googleClientID:
-    "641477541604-mc97danlfoi6pi7387tif4rfu17u6dh9.apps.googleusercontent.com",
+    "641477541604-mc97danlfoi6pi7387tif4rfu17u6dh9.apps.googleusercontent.com" ||
+    "641477541604-9q6l4r921odhbbg3c12gr16b6cj8doh9.apps.googleusercontent.com",
   googleClientSecret: "uvGpDyikUAk7P7o5zNuGyW0y",
   mongoURI:
-    "mongodb+srv://shimul:shimul07@cluster0-aweic.mongodb.net/test?retryWrites=true&w=majority",
+    "mongodb+srv://shimul:shimul07@cluster0-aweic.mongodb.net/test?retryWrites=true&w=majority" ||
+    "r4dxUHe2gU57ULemZ0r9uUwm",
   cookieKey: "ablsbfsbfbsladfohsfbaskdfajbgf"
 };
 
--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID:
-    "1064369858124-1pntvgj6480lm3s0ohigg6ke7le63b3g.apps.googleusercontent.com",
-  googleClientSecret: "d8v2WYIXYtKFyfxPPlwGMH3T",
+    "641477541604-9q6l4r921odhbbg3c12gr16b6cj8doh9.apps.googleusercontent.com",
+  googleClientSecret: "r4dxUHe2gU57ULemZ0r9uUwm",
   mongoURI:
     "mongodb+srv://shimul:shimul07@cluster0-aweic.mongodb.net/test?retryWrites=true&w=majority",
   cookieKey: "akbdnkfjjafuavgnejeknknlnlnmckKJKB"
