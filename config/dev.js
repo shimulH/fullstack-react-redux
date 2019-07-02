e647c41dbc30a589d1002d57c5c8b36e9534585c
shimulH <theshimulh@gmail.com>
2019-07-02 10:02:03 +0600

    Auth Flow change6

--- config/dev.js
+++ config/dev.js
@@ -2,10 +2,9 @@ module.exports = {
   googleClientID:
     "641477541604-mc97danlfoi6pi7387tif4rfu17u6dh9.apps.googleusercontent.com" ||
     "641477541604-9q6l4r921odhbbg3c12gr16b6cj8doh9.apps.googleusercontent.com",
-  googleClientSecret: "uvGpDyikUAk7P7o5zNuGyW0y",
+  googleClientSecret: "uvGpDyikUAk7P7o5zNuGyW0y" || "r4dxUHe2gU57ULemZ0r9uUwm",
   mongoURI:
-    "mongodb+srv://shimul:shimul07@cluster0-aweic.mongodb.net/test?retryWrites=true&w=majority" ||
-    "r4dxUHe2gU57ULemZ0r9uUwm",
+    "mongodb+srv://shimul:shimul07@cluster0-aweic.mongodb.net/test?retryWrites=true&w=majority",
   cookieKey: "ablsbfsbfbsladfohsfbaskdfajbgf"
 };
 
