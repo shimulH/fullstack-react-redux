5b8a256e08e383f71ce7d21e9b15dc3dabcef59d
shimulH <theshimulh@gmail.com>
2019-07-02 09:07:17 +0600

    Auth Flow

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./prod");
+} else {
+  module.exports = require("./dev");
+}
+//cID 1064369858124-1pntvgj6480lm3s0ohigg6ke7le63b3g.apps.googleusercontent.com
+//secID d8v2WYIXYtKFyfxPPlwGMH3T
